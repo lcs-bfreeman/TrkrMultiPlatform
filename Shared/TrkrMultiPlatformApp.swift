ffb869f381d8a3dceae8777e226d7aacc48964fe
Benjamn <bfreeman@lcs.on.ca>
2022-01-18 09:26:30 -0500

    Initial Commit

---
+++ Shared/TrkrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrkrMultiPlatformApp.swift
+//  Shared
+//
+//  Created by Ben Freeman on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrkrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
